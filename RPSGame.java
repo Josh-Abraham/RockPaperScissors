ff2c8f3261d7a7e4950360ce77ffc2587115e7ad
Josh Abraham <josh.m.n.abraham@gmail.com>
2018-01-24 23:43:33 -0500

    Initial commit

---
+++ RPSGame.java
@@ -0,0 +1,32 @@
+package eecs2030.lab5;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * The class that contains the main method that creates the rock, paper,
+ * scissors application.
+ *
+ */
+public class RPSGame {
+
+	/**
+	 * This method creates an RPSModel, RPSController, and RPSView to create the
+	 * rock, paper, scissors game. You need to set the model and view on the
+	 * controller, and you need to make the view visible.
+	 * 
+	 * @param args
+	 *            not used
+	 */
+
+	public static void main(String[] args) {
+
+		RPSModel model = new RPSModel(0);
+		RPSController controller = new RPSController();
+		RPSView view = new RPSView(controller);
+		controller.setModel(model);
+		controller.setView(view);
+		view.setVisible(true);
+
+	}
+}
